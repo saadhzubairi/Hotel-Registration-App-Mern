31a87e961cb279c19b5fd214a434b2ea9476b007
= <=>
2022-06-13 23:11:12 -0700

    comm

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navbar/Navbar.jsx";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/properyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = ()=>{
     return (
@@ -13,6 +14,8 @@ const Home = ()=>{
                 <Featured/>
                 <h className="homeTitle">Browse by property types</h>
                 <PropertyList/>
+                <h className="homeTitle">Homes guests love</h>
+                <FeaturedProperties/>
             </div>
         </div>
     )
