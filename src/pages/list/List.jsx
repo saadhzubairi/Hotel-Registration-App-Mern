68ed3e006fc8bc01cbd2bee8067bdf5660a73d6a
= <=>
2022-06-14 22:57:18 -0700

    commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -4,9 +4,13 @@ import Navbar from "../../components/navbar/Navbar.jsx";
 import { useLocation } from "react-router-dom"
 import { useState } from "react";
 import { format } from "date-fns";
+import { DateRange } from "react-date-range";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const List = () => {
     const location = useLocation()
+    const [openDate, setOpenDate] = useState(false);
 
     const [destination, setDestination] = useState(location.state.destination)
     const [date, setDate] = useState(location.state.date)
@@ -24,15 +28,48 @@ const List = () => {
                             <label>DESTINATION</label>
                             <input type="text" />
                         </div>
-                        <div className="lsItem">
+                        <div className="lsItem" onClick={() => setOpenDate(!openDate)}>
                             <label >DATE</label>
-                            <span className="dt"> {`${format(date[0].startDate, "dd/MM/yy")} - 
-                                    ${format(date[0].endDate, "dd/MM/yy")}`}</span>
+                            <span className="dt">
+                                {`${format(date[0].startDate, "dd/MM/yy")} - ${format(date[0].endDate, "dd/MM/yy")}`}
+                            </span>
+                            <DateRange
+                                editableDateInputs={true}
+                                onChange={item => setDate([item.selection])}
+                                moveRangeOnFirstSelection={false}
+                                ranges={date}
+                                className={openDate ? "date" : "date hide"}
+                            />
                         </div>
                         <div className="lsItem">
                             <label>OPTIONS</label>
-                            <input type="text" />
                         </div>
+                        <div className="lsOpItems">
+                            <div className="lsOptionItem">
+                                <div className="lsOpText">Min Price</div>
+                                <input type="number" className="lsOpInput" />
+                            </div>
+                            <div className="lsOptionItem">
+                                <div className="lsOpText">Max Price</div>
+                                <input type="number" className="lsOpInput" />
+                            </div>
+                            <div className="lsOptionItem">
+                                <div className="lsOpText">Adults</div>
+                                <input type="number" className="lsOpInput" ></input>
+                            </div>
+                            <div className="lsOptionItem">
+                                <div className="lsOpText">Children</div>
+                                <input type="number" className="lsOpInput" />
+                            </div>
+                            <div className="lsOptionItem">
+                                <div className="lsOpText">Room</div>
+                                <input type="number" className="lsOpInput" />
+                            </div>
+                        </div>
+                        <button className="searchBt">
+                            <FontAwesomeIcon className="searchBt scbt" icon={faSearch} />
+                            SEARCH
+                        </button>
                     </div>
                     <div className="listResult"></div>
                 </div>
